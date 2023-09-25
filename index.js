6904692171edd0f326695354cef793f6e66b0f18
Gustavo Stierli <gstierli@teco.com.ar>
2023-09-25 12:46:18 -0300

    less

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const sum = require('./sum');
 const less = require('./less');
 
 router.get('/add/:a/:b', (ctx, next) => {
-  const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
-  return ctx.body = { result };
+  const result_ADD = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
+  return ctx.body = { result_ADD };
 });
 router.get('/less/:a/:b', (ctx, next) => {
-   const result1 = less(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
-   return ctx.body = { result1 };
+   const result_LESS = less(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
+   return ctx.body = { result_LESS };
 });
 app
   .use(router.routes())
