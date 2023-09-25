1ea8d6c29d5e3ac340146c8d009a14f5120c5534
Gustavo Stierli <gstierli@teco.com.ar>
2023-09-25 20:05:53 -0300

    update

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 const sum = require('./sum');
+const less = require('./less');
 
 router.get('/add/:a/:b', (ctx, next) => {
-  const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
-  return ctx.body = { result };
+  const result_ADD = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
+  return ctx.body = { result_ADD };
 });
-
+router.get('/less/:a/:b', (ctx, next) => {
+  const result_LESS = less(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
+  return ctx.body = { result_LESS };
+});
 app
   .use(router.routes())
   .use(router.allowedMethods());
